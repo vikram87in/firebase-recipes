a09a3b66fc7d681225414282eb42856b29564c78
vikram singh <vikram.singh@bold.com>
2023-08-14 22:43:22 +0530

    login and logout functionality implemented; reset password mail also implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import './FirebaseConfig';
+import FirebaseAuthService from './FirebaseAuthService';
+import LoginForm from './components/LoginForm';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    FirebaseAuthService.subscribeToAuthChanges(setUser);
+  }, []);
   return (
     <div className="App">
       <div className='App'>
         <div className='title-row'>
           <h1 className='title'>Firebase Recipes</h1>
+          <LoginForm user={user} />
         </div>
       </div>
     </div>
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import FirebaseAuthService from '../FirebaseAuthService';
+
+export default function LoginForm({ user }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogout = () => {
+    FirebaseAuthService.logoutUser();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await FirebaseAuthService.loginUser(username, password);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      alert(`code: ${error.code}; message: ${error.message}`);
+    }
+  };
+
+  const handleSendResetPasswordEmail = async () => {
+    if (!username) {
+      alert('missing username');
+      return;
+    }
+    try {
+      await FirebaseAuthService.sendPasswordResetEmail(username);
+      alert('password reset mail sent!');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const renderHeadingForAuthUser = <div className='row'>
+    <h3>Welcome, {user?.email}</h3>
+    <button type='button' className='primary-button' onClick={handleLogout}>
+      Logout
+    </button>
+  </div>;
+
+
+  const renderForm = <form onSubmit={handleSubmit} className='login-form'>
+    <label className='input-label login-label'>
+      Username (email):
+      <input
+        type="email"
+        required
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+        className='input-text' />
+    </label>
+    <label className='input-label login-label'>
+      Password:
+      <input
+        type="password"
+        required
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        className='input-text' />
+    </label>
+    <div className='button-box'>
+      <button className='primary-button'>Login</button>
+      <button type='button' onClick={handleSendResetPasswordEmail}>Reset Password</button>
+    </div>
+
+  </form>;
+
+  return (
+    <div className='login-form-container'>
+      {
+        user
+          ? renderHeadingForAuthUser
+          : renderForm
+      }
+    </div>
+  );
+}
